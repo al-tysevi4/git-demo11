9dbb558b835b5e1c161a10f9a4362b05501870ae
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-14 18:35:36 +0300

    Initial commit

---
+++ sandbox/src/main/java/task1/SimpleProgram.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class SimpleProgram {
+    public static void main(String[] args) {
+      sayHello("world");
+      sayHello("Alex");
+    }
+    public static void sayHello(String s) {
+        System.out.println("Hello, " + s + "!");
+    }
+}
